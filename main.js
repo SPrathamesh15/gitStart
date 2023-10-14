04fcd0d67c7c3d463096b6a5fb8f874c1641f6cf
SPrathamesh15 <sprathamesh354@gmail.com>
2023-10-14 19:06:37 +0530

    adding filter

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ form.addEventListener('submit', addItem)
 // delete event
 itemList.addEventListener('click', removeItem);
 
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
 // Add Item
 function addItem(e){
     e.preventDefault();
@@ -46,4 +49,21 @@ function removeItem(e){
         itemList.removeChild(li);
       }
     }
+  }
+
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
   }
